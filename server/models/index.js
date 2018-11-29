af81945e2067eabd0429e58c7f9a372abb9da23d
Scott Robertson <scottrobertson97@gmail.com>
2018-11-29 10:17:13 -0500

    renaming old stuff

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,3 @@
 module.exports.Account = require('./Account.js');
-module.exports.Domo = require('./Domo.js');
-module.exports.Drink = require('./Drink.js');
+// module.exports.Domo = require('./Domo.js');
+// module.exports.Drink = require('./Drink.js');
