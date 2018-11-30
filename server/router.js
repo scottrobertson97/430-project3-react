ad7babf51244738beeae5852612c2536bec5d331
Scott Robertson <scottrobertson97@gmail.com>
2018-11-30 14:56:17 -0500

    eslint errors fixed

--- server/router.js
+++ server/router.js
@@ -12,20 +12,44 @@ const router = (app) => {
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
   app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
   app.get('/logout', mid.requiresSecure, mid.requiresLogin, controllers.Account.logout);
-  //main page
-  app.get('/app', mid.requiresSecure, mid.requiresLogin, controllers.Transaction.addTransactionPage);
-  app.post('/app', mid.requiresSecure, mid.requiresLogin, controllers.Transaction.addTransaction);
+  // main page
+  app.get(
+    '/app',
+    mid.requiresSecure,
+    mid.requiresLogin,
+    controllers.Transaction.addTransactionPage
+  );
+  app.post(
+    '/app',
+    mid.requiresSecure,
+    mid.requiresLogin,
+    controllers.Transaction.addTransaction
+  );
   // add transactions
-  app.get('/addTransaction', mid.requiresSecure, mid.requiresLogin, controllers.Transaction.addTransactionPage);
-  app.post('/addTransaction', mid.requiresSecure, mid.requiresLogin, controllers.Transaction.addTransaction);
-  app.get('/getTransactions', mid.requiresSecure, mid.requiresLogin, controllers.Transaction.getTransactions);
+  app.get(
+    '/addTransaction',
+    mid.requiresSecure,
+    mid.requiresLogin,
+    controllers.Transaction.addTransactionPage
+  );
+  app.post(
+    '/addTransaction',
+    mid.requiresSecure,
+    mid.requiresLogin,
+    controllers.Transaction.addTransaction
+  );
+  app.get(
+    '/getTransactions',
+    mid.requiresSecure,
+    mid.requiresLogin,
+    controllers.Transaction.getTransactions
+  );
   //
   //
   // app.get('/getDomos', mid.requiresSecure, mid.requiresLogin, controllers.Domo.getDomos);
   // app.get('/getDrinks', mid.requiresSecure, mid.requiresLogin, controllers.Drink.getDrinks);
   // app.get('/maker', mid.requiresSecure, mid.requiresLogin, controllers.Drink.makerPage);
   // app.post('/maker', mid.requiresSecure, mid.requiresLogin, controllers.Drink.make);
-  // app.get('/getAllDrinks', mid.requiresSecure, mid.requiresLogin, controllers.Drink.getAllDrinks);
   // app.get('/allDrinks', mid.requiresSecure, mid.requiresLogin, controllers.Drink.allDrinksPage);
 };
 
